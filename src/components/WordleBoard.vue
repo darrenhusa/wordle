fe3308dc901973b620ca2c96d4fd1c3a2ac50d95
Darren Henderson <darrenh@ccsj.edu>
2025-01-04 23:41:33 -0600

    Finished video 8.

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,21 +1,46 @@
 <script setup lang="ts">
-import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '@/settings';
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE, WORD_SIZE } from '@/settings';
 import englishWords from "@/englishWordsWith5Letters.json"
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 defineProps({
   wordOfTheDay: {
     type: String,
-    validator: (wordGiven: string) => wordGiven.length === 5
+    validator: (wordGiven: string) => wordGiven.length === WORD_SIZE
     && wordGiven.toUpperCase() === wordGiven && englishWords.includes(wordGiven)
   }
 })
 const guessInProgress = ref("")
 const guessSubmitted = ref("")
+
+const formattedGuessInProgress = computed({
+  get(){
+    return guessInProgress.value
+  },
+  set(rawValue: string){
+    guessInProgress.value = rawValue
+        .slice(0, WORD_SIZE)
+        .toUpperCase()
+        .replace(/[^A-Z]+/gi, "")
+  },
+
+})
+
+function onSubmit() {
+  if (!englishWords.includes(guessInProgress.value)) {
+    return;
+  }
+  guessSubmitted.value = guessInProgress.value
+
+}
 </script>
 
 <template>
-  <input type="text" v-model="guessInProgress" @keydown.enter="guessSubmitted = guessInProgress">
+  <input v-model="formattedGuessInProgress"
+         type="text"
+         :maxlength="WORD_SIZE"
+         @keydown.enter="onSubmit"
+  >
   <p v-if="guessSubmitted.length > 0"
      v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE" />
 </template>
